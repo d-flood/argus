d162d7a3ccc9a8597b75ec9d5983cae15e933447
David Flood <69060117+d-flood@users.noreply.github.com>
2024-09-09 21:33:34 -0500

    initialize django

---
+++ django/core/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
+from django.views.decorators.http import require_safe
+
+
+@require_safe
+def dashboard(request: HttpRequest) -> HttpResponse:
+    return render(request, "dashboard.html")
